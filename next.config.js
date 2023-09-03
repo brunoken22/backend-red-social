0c37c7efc399b9ee652723aa10cc1d1bd6c1ee19
Bruno Ken <bruno_am_22@hotmail.com>
2023-09-03 15:10:54 -0300

    mas

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    compilerOptions: {
+    "compilerOptions": {
         "typeRoots": ["./types", "./node_modules/@types"]
       },
     experimental: {
